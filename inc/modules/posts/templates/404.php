69464673cf15f211bbdd3875bfd300148f9c2706
bahrul <bahrul@bahrul-PC>
2017-02-09 11:06:28 +0800

    base sass for posts and comments

--- inc/modules/posts/templates/404.php
+++ inc/modules/posts/templates/404.php
@@ -3,7 +3,7 @@
  * Template part for displaying 404 pages (not found)
  */
 ?>
-<section class="error-404 not-found">
+<section class="error-404">
     
     <?php mbtheme_module_template( 'posts', 'contents/content', 'none' ); ?>
 
