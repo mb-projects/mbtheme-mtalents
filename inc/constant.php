feaf8977537aa195192e38413541e28adb626826
bahrul221 <bahrul221mb@gmail.com>
2017-02-27 02:53:49 +0800

    Add core dir and apply shortcode on widget text output

--- inc/constant.php
+++ inc/constant.php
@@ -4,9 +4,11 @@
 define( 'MBTHEME_DIR', get_template_directory() );
 define( 'MBTHEME_DIR_ASSETS', MBTHEME_DIR . '/assets' );
 define( 'MBTHEME_DIR_INC', MBTHEME_DIR . '/inc' );
+define( 'MBTHEME_DIR_CORE', MBTHEME_DIR_INC . '/core' );
 define( 'MBTHEME_DIR_MODULES', MBTHEME_DIR_INC . '/modules' );
 
 define( 'MBTHEME_URI', get_template_directory_uri() );
 define( 'MBTHEME_URI_ASSETS', MBTHEME_URI . '/assets' );
 define( 'MBTHEME_URI_INC', MBTHEME_URI . '/inc' );
+define( 'MBTHEME_URI_CORE', MBTHEME_URI_INC . '/core' );
 define( 'MBTHEME_URI_MODULES', MBTHEME_URI_INC . '/modules' );
--- inc/loader.php
+++ inc/loader.php
@@ -3,6 +3,29 @@
 /**
  * loader
  */
+
+// load functions of core
+foreach ( glob( MBTHEME_DIR_CORE . '/*/' ) as $module_load ) {
+    if ( file_exists( $module_load . 'functions.php' ) ) {
+        include_once $module_load . 'functions.php';
+    }    
+}
+// load all main files of core
+foreach ( glob( MBTHEME_DIR_CORE . '/*/' ) as $module_load ) {
+    if ( file_exists( $module_load . 'load.php' ) ) {
+        include_once $module_load . 'load.php';
+    }
+    if ( file_exists( $module_load . 'hooks.php' ) ) {
+        include_once $module_load . 'hooks.php';
+    }
+    if ( file_exists( $module_load . 'options.php' ) ) {
+        include_once $module_load . 'options.php';
+    }
+    if ( file_exists( $module_load . 'shortcodes.php' ) ) {
+        include_once $module_load . 'shortcodes.php';
+    }
+}
+
 /**
  * load all functions files of modules
  */
