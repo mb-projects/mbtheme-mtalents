23962e4cd642ebbab18d8e9bd47ca7f6cda8e9c2
bahrul221 <bahrul221mb@gmail.com>
2017-03-02 16:11:22 +0800

    Add custom editor widget and vertical middle cell

---
+++ inc/modules/siteorigin-panels/load.php
@@ -0,0 +1,5 @@
+<?php
+
+$dir = dirname( __FILE__ );
+
+include_once $dir . '/widgets/mtalents-editor/mtalents-editor.php';
---
+++ inc/modules/siteorigin-panels/widgets/mtalents-editor/mtalents-editor.php
@@ -0,0 +1,37 @@
+<?php
+
+if ( !class_exists( 'SiteOrigin_Widget' ) )
+	return;
+
+class MBTheme_Mtalents_Editor_Widget extends SiteOrigin_Widget {
+
+	function __construct() {
+		parent::__construct( 'mtalents-editor', 'MTalents Editor', array( 'description' => '' ), array(), array(), plugin_dir_path( __FILE__ ) );
+	}
+
+	function get_template_name( $instance ) {
+		return 'mtalents-editor';
+	}
+
+	function get_style_name( $instance ) {
+		return 'tpl';
+	}
+
+	function get_widget_form() {
+		$fields = array(
+			'title'		 => array(
+				'type'	 => 'text',
+				'label'	 => 'Title',
+			),
+			'content'	 => array(
+				'type'	 => 'tinymce',
+				'label'	 => 'Content',
+			),
+		);
+
+		return $fields;
+	}
+
+}
+
+siteorigin_widget_register( 'mtalents-editor', __FILE__, 'MBTheme_Mtalents_Editor_Widget' );
