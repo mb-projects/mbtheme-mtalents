19db8e3cf630da2c86a315e868d465059ea27139
bahrul221 <bahrul221mb@gmail.com>
2017-03-03 22:53:27 +0800

    Right image position for media widget

--- inc/modules/siteorigin-panels/widgets/mtalents-media/tpl/mtalents-media.php
+++ inc/modules/siteorigin-panels/widgets/mtalents-media/tpl/mtalents-media.php
@@ -1,8 +1,9 @@
 <?php
 $image_id	 = $instance[ 'image' ];
 $image		 = wp_get_attachment_image( $image_id, 'full', false, array( 'class' => 'mtalents-media-img' ) );
+$pos		 = $instance[ 'image_pos' ];
 ?>
-<div class="mtalents-media">
+<div class="mtalents-media <?php echo ($pos === 'right') ? 'mtalents-media-right' : ''; ?>">
 	<div class="mtalents-media-image">
 		<div class="img-border img-border-bl">
 			<?php echo $image; ?>
@@ -13,4 +14,11 @@ $image		 = wp_get_attachment_image( $image_id, 'full', false, array( 'class' => 
 			<?php echo $instance[ 'content' ]; ?>
 		</div>
 	</div>
+	<?php if ( $pos === 'right' ) { ?>
+		<div class="mtalents-media-image mtalents-media-image-right">
+			<div class="img-border img-border-br">
+				<?php echo $image; ?>
+			</div>
+		</div>
+	<?php } ?>
 </div>
