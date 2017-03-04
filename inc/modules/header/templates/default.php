663de3951feab9d1b0b9e1c8e70374874a4d6519
bahrul221 <bahrul221mb@gmail.com>
2017-03-04 21:39:58 +0800

    Header background form single blog

--- inc/modules/header/templates/default.php
+++ inc/modules/header/templates/default.php
@@ -1,18 +1,18 @@
 <?php
 // get page thumbnail image if it is a page
+$page_id = get_queried_object_id(); 
 $bg_url = '';
-if ( is_home() ) {
-    $bg     = mbtheme_get_option( 'header-bg-blog', array() );
-    $bg_url = $bg[ 'url' ];
-} elseif ( is_page() ) {
-    $page_id = get_queried_object_id();
-    $bg_url  = get_the_post_thumbnail_url( $page_id, 'full' );
+if ( is_singular( 'post' ) ) {
+	$bg		 = mbtheme_get_option( 'header-bg-blog', array() );
+	$bg_url	 = $bg[ 'url' ];
+} elseif ( (is_home() || is_page()) && $page_id ) {
+	$bg_url	 = get_the_post_thumbnail_url( $page_id, 'full' );
 }
 
 // get default backgound image
 if ( !$bg_url ) {
-    $bg     = mbtheme_get_option( 'header-bg-default', array() );
-    $bg_url = $bg[ 'url' ];
+	$bg		 = mbtheme_get_option( 'header-bg-default', array() );
+	$bg_url	 = $bg[ 'url' ];
 }
 
 // set bg style
@@ -20,6 +20,6 @@ $bg_style = ($bg_url) ? 'background-image: url("' . esc_url( $bg_url ) . '")' : 
 ?>
 <div class="site-header" style="<?php echo esc_attr( $bg_style ); ?>">
     <div class="site-header-inner">
-        <?php do_action( 'mbtheme_site_header_inner' ); ?>
+		<?php do_action( 'mbtheme_site_header_inner' ); ?>
     </div>
 </div>
